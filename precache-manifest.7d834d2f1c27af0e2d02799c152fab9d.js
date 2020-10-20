922e2828ac2496814661b84e35bbda803458b6cb
github username <github email address>
2020-10-20 14:05:03 -0400

    Updates

---
+++ precache-manifest.7d834d2f1c27af0e2d02799c152fab9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "327f4abe01545aef6df4cad3efc31e86",
+    "url": "/hamcodes/todo-app/index.html"
+  },
+  {
+    "revision": "b20bfa0f2929e009b875",
+    "url": "/hamcodes/todo-app/static/css/main.0e6108ec.chunk.css"
+  },
+  {
+    "revision": "25244afc0e9f5e5fa21f",
+    "url": "/hamcodes/todo-app/static/js/2.1fc706e1.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/hamcodes/todo-app/static/js/2.1fc706e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b20bfa0f2929e009b875",
+    "url": "/hamcodes/todo-app/static/js/main.6ddd6a21.chunk.js"
+  },
+  {
+    "revision": "0315370541fec0c2a5ad",
+    "url": "/hamcodes/todo-app/static/js/runtime-main.16856fcb.js"
+  }
+]);
